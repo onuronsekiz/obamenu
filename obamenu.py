438a65225e078064dd95e65fdbc1374ecf265499
onuronsekiz <76511536+onuronsekiz@users.noreply.github.com>
2020-12-23 19:58:31 +0300

    Update obamenu.py

--- obamenu.py
+++ obamenu.py
@@ -34,7 +34,7 @@ except IOError:
 #selected_theme = "Papirus" #direct theme selection 
 application_groups = ("AudioVideo", "Development", "Editors",  "Engineering", "Games", "Graphics", "Internet",  "Multimedia", "Office",  "Other",  "Settings", "System",  "Utilities") # enter here new category as you wish, it will be sorted 
 group_aliases = {"Audio":"Multimedia","Video":"Multimedia","AudioVideo":"Multimedia","Network":"Internet","Game":"Games", "Utility":"Utilities", "Development":"Editors","GTK":"",  "GNOME":""}
-ignoreList = ("gtk3-icon-browser","evince-previewer", "Ted",  "wingide3.2", "python3.4", "feh","xfce4-power-manager-settings" )
+ignoreList = ("gtk3-icon-browser","evince-previewer", "Ted",  "wingide3.2", "python3.4", "feh","xfce4-power-manager-settings","picom","compton" )
 prefixes = ("legacy","categories","apps","devices","mimetypes","places","preferences","actions", "status","emblems") #added for prefered icon dirs and sizes. could be gathered automatically but wouldn't be sorted like this
 iconSizes = ("48","32","24","16","48x48","40x40","36x36","32x32","24x24","64x64","72x72","96x96","16x16","128x128","256x256","scalable","apps","symbolic")
 terminal_string = "qterminal -e"         # your favourites terminal exec string
@@ -119,7 +119,7 @@ class dtItem(object):
 		#else a short name like "myapp"
 		tmp = glob.glob(image_dir + di + ".*")
 		if len(tmp) == 0: #if there is not correct icon in pixmap, check for icon theme
-			for index, theme in enumerate(iconThemes):
+			for theme in iconThemes:
 				tmp=[s for s in iconList if di in s] #check program name in icon list
 				if len(tmp) > 0:
 					break # end loop if found
